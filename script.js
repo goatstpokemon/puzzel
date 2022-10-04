8f2459c0d1f6971b2c97e6cd9315d40663024d65
goatstpokemon <github@lukedixon.nl>
2022-10-04 17:12:01 +0200

    added dialog

--- script.js
+++ script.js
@@ -1,40 +1,57 @@
+var dialog = document.querySelector('dialog');
+function openDialog() {
+  dialog.showModal();
+}
+function closeDialog() {
+  dialog.close();
+}
+
 const grid = document.querySelector('.swipe-box');
 const wh = 4;
 var gridSquare = [];
 let tiles = [
+  // Row 1
+  'url(img/puzzel-4.png)',
+  'url(img/puzzel-7.png)',
+  'url(img/puzzel-8.png)',
   'url(img/puzzel-0.png)',
-  'url(img/puzzel-0.png)',
-  'url(img/puzzel-0.png)',
-  'url(img/puzzel-0.png)',
-  'url(img/puzzel-0.png)',
-  'url(img/puzzel-1.png)',
-  'url(img/puzzel-0.png)',
-  'url(img/puzzel-1.png)',
-  'url(img/puzzel-1.png)',
-  'url(img/puzzel-1.png)',
-  'url(img/puzzel-1.png)',
-  'url(img/puzzel-0.png)',
+  // Row 2
   'url(img/puzzel-0.png)',
   'url(img/puzzel-1.png)',
+  'url(img/puzzel-7.png)',
+  'url(img/puzzel-4.png)',
+  // Row 3
+  'url(img/puzzel-3.png)',
+  'url(img/puzzel-2.png)',
+  'url(img/puzzel-2.png)',
+  'url(img/puzzel-0.png)',
+  // Row 4
+  'url(img/puzzel-0.png)',
+  'url(img/puzzel-8.png)',
   'url(img/puzzel-1.png)',
-  'url(img/puzzel-1.png)',
+  'url(img/puzzel-3.png)',
 ];
 const init = () => {
   gridSquare = [];
+  openDialog();
   for (let i = 0; i < wh * wh; i++) {
     let tile = document.createElement('div');
     tile.setAttribute('draggable', true);
     tile.setAttribute('id', i);
+    if (i === 2 || i === 13) {
+      tile.setAttribute('draggable', false);
+    }
     tile.style.backgroundImage = `${tiles[i]}`;
     tile.style.backgroundSize = '100%';
+    grid.appendChild(tile);
+    gridSquare.push(tile);
+
     tile.addEventListener('dragstart', dragStart);
     tile.addEventListener('dragend', dragEnd);
     tile.addEventListener('dragover', dragOver);
     tile.addEventListener('dragenter', dragEnter);
     tile.addEventListener('drageleave', dragLeave);
     tile.addEventListener('drop', dragDrop);
-    grid.appendChild(tile);
-    gridSquare.push(tile);
   }
 };
 
@@ -59,10 +76,10 @@ function dragEnter(e) {
   e.preventDefault();
 }
 function dragLeave() {
-  this.style.background = '';
+  this.style.backgroundImage = '';
 }
 function dragDrop() {
-  imageBeingReplaced = `${this.style.background}  no-repeat center`;
+  imageBeingReplaced = this.style.backgroundImage;
   tileBeingReplaced = parseInt(this.id);
 }
 /* 
