679c3432077aac1568b2b7b24701f9c3669b46f4
Luke Dixon <github@lukedixon.nl>
2022-10-31 11:26:47 +0100

    added puzzel1

--- script.js
+++ script.js
@@ -1,271 +1,64 @@
-// ----------------------------- SETUP ------------------------------------------------------------
-var dialog = document.querySelector('.modal');
-var winDialog = document.querySelector('.winmodal');
-const grid = document.querySelector('.swipe-box');
-const ignore = document.querySelector('.ignore');
-const wh = 4;
-var gridSquare = [];
-const body = document.querySelector('body');
-let endTiles = [
-  // Row 1
-  'url("img/puzzel-4.png")',
-  'url("img/puzzel-7.png")',
-  'url("img/puzzel-8.png")',
-  'url("img/puzzel-0.png")',
-  // Row 2
-  'url("img/puzzel-0.png")',
-  'url("img/puzzel-1.png")',
-  'url("img/puzzel-7.png")',
-  'url("img/puzzel-4.png")',
-  // Row 3
-  'url("img/puzzel-3.png")',
-  'url("img/puzzel-2.png")',
-  'url("img/puzzel-2.png")',
-  'url("img/puzzel-0.png")',
-  // Row 4
-  'url("img/puzzel-0.png")',
-  'url("img/puzzel-8.png")',
-  'url("img/puzzel-1.png")',
-  'url("img/puzzel-3.png")',
-];
-let startTiles = [
-  // Row 1
-  'url(img/puzzel-7.png)',
-  'url(img/puzzel-4.png)',
-  'url(img/puzzel-8.png)',
-  'url(img/puzzel-0.png)',
-
-  // Row 2
-  'url(img/puzzel-7.png)',
-  'url(img/puzzel-2.png)',
-  'url(img/puzzel-7.png)',
-  'url(img/puzzel-0.png)',
-  // Row 3
-  'url(img/puzzel-3.png)',
-  'url(img/puzzel-2.png)',
-  'url(img/puzzel-1.png)',
-  'url(img/puzzel-0.png)',
-  // Row 4
-  'url(img/puzzel-4.png)',
-  'url(img/puzzel-8.png)',
-  'url(img/puzzel-1.png)',
-  'url(img/puzzel-3.png)',
-];
-let imageBeingDragged;
-let imageBeingReplaced;
-let tileBeingDragged;
-let tileBeingReplaced;
-
-// --------------------------------------- Desktop -----------------------------------------------
-const init = () => {
-  openDialog();
-  for (let i = 0; i < wh * wh; i++) {
-    let tile = document.createElement('div');
-    tile.setAttribute('id', i);
-    tile.style.backgroundImage = `${startTiles[i]}`;
-    tile.style.backgroundSize = '100%';
-    if (
-      tile.style.backgroundImage.toString().toUpperCase() ===
-      'url("img/puzzel-8.png")'.toString().toUpperCase()
-    ) {
-      tile.setAttribute('draggable', false);
+const nextButton = document.querySelectorAll('.volgende');
+const dialogs = document.querySelectorAll('.second');
+var counter = 0;
+const startingMinutes = 2;
+let time = startingMinutes * 60;
+const countdownEL = document.getElementById('countdown');
+let score = -1;
+let scorePlayer = document.getElementById('score-player');
+const box = document.querySelectorAll('.box');
+function nextText() {
+  let e = document.getElementById('two');
+  counter += 1;
+  console.log(counter);
+  for (i = 0; i < dialogs.length; i++) {
+    if (i === counter) {
+      dialogs[i].classList.remove('hidden');
     } else {
-      tile.setAttribute('draggable', true);
-      tile.addEventListener('dragstart', dragStart);
-      tile.addEventListener('dragend', dragEnd);
-      tile.addEventListener('dragover', dragOver);
-      tile.addEventListener('dragenter', dragEnter);
-      tile.addEventListener('drageleave', dragLeave);
-      tile.addEventListener('drop', dragDrop);
+      dialogs[i].classList.add('hidden');
     }
-    grid.appendChild(tile);
-    gridSquare.push(tile);
   }
-};
-
-function dragStart() {
-  imageBeingDragged = this.style.backgroundImage;
-  tileBeingDragged = parseInt(this.id);
+  if (counter === 5) {
+    e.scrollIntoView({
+      block: 'end',
+      behavior: 'smooth',
+      inline: 'center',
+    });
+    setInterval(updateCountdown, 1000);
+  }
 }
-function dragEnd() {
-  const validMoves = [
-    tileBeingDragged - wh,
-    tileBeingDragged + wh,
-    tileBeingDragged + 1,
-    tileBeingDragged - 1,
-  ];
-  let validMove = validMoves.includes(tileBeingReplaced);
-  if (tileBeingReplaced && validMove) {
-    gridSquare[tileBeingReplaced].style.backgroundImage = imageBeingDragged;
-    gridSquare[tileBeingDragged].style.backgroundImage = imageBeingReplaced;
-    tileBeingReplaced = null;
+
+function updateCountdown() {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+
+  countdownEL.innerHTML = minutes + ':' + seconds;
+  if (minutes == 0 && seconds == 0) {
   } else {
-    gridSquare[tileBeingDragged].style.backgroundImage = tileBeingDragged;
+    time--;
   }
-  won();
 }
-function dragOver(e) {
-  e.preventDefault();
-}
-function dragEnter(e) {
-  e.preventDefault();
-}
-function dragLeave() {
-  this.style.backgroundImage = '';
-}
-function dragDrop() {
-  imageBeingReplaced = this.style.backgroundImage;
-  tileBeingReplaced = parseInt(this.id);
-}
-// --------------------------------------- Mobile -----------------------------------------------
-const initMobile = () => {
-  for (let i = 0; i < wh * wh; i++) {
-    let tile = document.createElement('div');
-    tile.setAttribute('id', i);
-    tile.style.backgroundImage = `${startTiles[i]}`;
-    tile.style.backgroundSize = '100%';
-    if (
-      tile.style.backgroundImage.toString().toUpperCase() ===
-      'url("img/puzzel-8.png")'.toString().toUpperCase()
-    ) {
-      tile.setAttribute('draggable', false);
-    } else {
-      tile.setAttribute('draggable', true);
-      tile.addEventListener('pointerdown', down);
-    }
-    grid.appendChild(tile);
-    gridSquare.push(tile);
-  }
-};
 
-function down(event) {
-  imageBeingDragged = this.style.backgroundImage;
-  tileBeingDragged = parseInt(this.id);
-  this.style.left = `${this.getBoundingClientRect().left}px`;
-  this.style.top = `${this.getBoundingClientRect().top}px`;
-
-  const clone = this.cloneNode();
-  this.before(clone);
-  this.classList.add('move');
-
-  // document.body.append(tileBeingDragged);
-  this.style.pointerEvents = 'none';
-  this.setPointerCapture(event.pointerId);
-
-  [...gridSquare].forEach((square) => {
-    square.addEventListener('pointermove', move);
-    square.addEventListener('pointerup', up);
-
-    if (parseInt(square.id) != parseInt(tileBeingDragged)) {
-      if (
-        square.style.backgroundImage.toString().toUpperCase() ===
-        'url("img/puzzel-8.png")'.toString().toUpperCase()
-      ) {
-        console.log('hello');
-        return;
-      } else {
-        square.setAttribute('placable', true);
-      }
-    } else {
-      return;
-    }
-  });
-
-  function move(event) {
-    this.style.left = `${parseFloat(this.style.left) + event.movementX}px`;
-    this.style.top = `${parseFloat(this.style.top) + event.movementY}px`;
-    const hitTest = document.elementFromPoint(
-      parseFloat(this.style.left),
-      parseFloat(this.style.top)
-    );
-
-    const dropzone = hitTest.closest('[placable]');
-    if (!dropzone) {
-      return;
-    }
-    imageBeingReplaced = dropzone.style.backgroundImage;
-    tileBeingReplaced = parseInt(dropzone.id);
-  }
-
-  function up(event) {
-    clone.remove();
-    this.style.top = '';
-    this.style.left = '';
-    this.classList.remove('move');
-    console.log({
-      tilebeingreplaced: tileBeingReplaced,
-      tileBeingDragged: tileBeingDragged,
-      imageBeingDragged: imageBeingDragged,
-      imageBeingReplaced: imageBeingReplaced,
+function changeColor() {
+  let win = document.getElementById('three');
+  for (let i = 0; i < box.length; i++) {
+    box[i].addEventListener('click', (e) => {
+      e.target.style.color = '#D86C46';
     });
-    const validMoves = [
-      tileBeingDragged - wh,
-      tileBeingDragged + wh,
-      tileBeingDragged + 1,
-      tileBeingDragged - 1,
-    ];
-    let validMove = validMoves.includes(tileBeingReplaced);
-    if (tileBeingReplaced && validMove) {
-      gridSquare[tileBeingReplaced].style.backgroundImage = imageBeingDragged;
-      gridSquare[tileBeingDragged].style.backgroundImage = imageBeingReplaced;
-      tileBeingReplaced = null;
-    } else {
-      gridSquare[tileBeingDragged].style.backgroundImage = tileBeingDragged;
-    }
-    this.style.pointerEvents = '';
-    this.releasePointerCapture(event.pointerId);
-    [...gridSquare].forEach((square) => {
-      square.removeAttribute('placable');
+  }
+  if (score !== 7) {
+    score += 1;
+    scorePlayer.textContent = score;
+  }
+  if (score === 7) {
+    win.scrollIntoView({
+      block: 'end',
+      behavior: 'smooth',
+      inline: 'center',
     });
-    won();
   }
-}
-// --------------------------------------- END Mobile -----------------------------------------------
 
-if ('ontouchstart' in document.documentElement) {
-  initMobile();
-} else {
-  init();
-}
-
-function reset() {
-  location.reload();
-}
-function won() {
-  if (
-    gridSquare[0].style.backgroundImage.toString().toUpperCase() ===
-      endTiles[0].toString().toUpperCase() &&
-    gridSquare[4].style.backgroundImage.toString().toUpperCase() ===
-      endTiles[4].toString().toUpperCase() &&
-    gridSquare[5].style.backgroundImage.toString().toUpperCase() ===
-      endTiles[5].toString().toUpperCase() &&
-    gridSquare[6].style.backgroundImage.toString().toUpperCase() ===
-      endTiles[6].toString().toUpperCase() &&
-    gridSquare[7].style.backgroundImage.toString().toUpperCase() ===
-      endTiles[7].toString().toUpperCase() &&
-    gridSquare[8].style.backgroundImage.toString().toUpperCase() ===
-      endTiles[8].toString().toUpperCase() &&
-    gridSquare[9].style.backgroundImage.toString().toUpperCase() ===
-      endTiles[9].toString().toUpperCase() &&
-    gridSquare[11].style.backgroundImage.toString().toUpperCase() ===
-      endTiles[11].toString().toUpperCase() &&
-    gridSquare[15].style.backgroundImage.toString().toUpperCase() ===
-      endTiles[15].toString().toUpperCase()
-  ) {
-    showwinDialog();
-  }
-}
-
-function openDialog() {
-  dialog.showModal();
-}
-function closeDialog() {
-  dialog.close();
-}
-
-function showwinDialog() {
-  winDialog.showModal();
-}
-function closeWinDialog() {
-  winDialog.close();
+  console.log(score);
 }
